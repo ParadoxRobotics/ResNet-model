079fecd32e0eff2d736f6f67fed864c3e6b099c3
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2019-04-03 15:18:45 +0200

    Update CIFAR10_ResNet.py

--- CIFAR10_ResNet.py
+++ CIFAR10_ResNet.py
@@ -35,12 +35,12 @@ transform_test = transforms.Compose(
 
 # load training dataset
 train_data = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-train_loader = data.DataLoader(train_data, batch_size = 64, shuffle = True)
+train_loader = data.DataLoader(train_data, batch_size = 128, shuffle = True)
 print("Number of train samples: ", len(train_data))
 print("Class : ", len(train_data.classes))
 # Load test dataset
 test_data = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-test_loader = data.DataLoader(test_data, batch_size = 64, shuffle = True)
+test_loader = data.DataLoader(test_data, batch_size = 128, shuffle = True)
 print("Number of test samples: ", len(test_data))
 print("Class : ", len(test_data.classes))
 
